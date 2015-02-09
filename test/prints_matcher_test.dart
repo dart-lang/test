c2756f84ab81216e908a8b92c866bffcbd28282a
Kevin Moore <kevmoo@google.com>
2015-02-09 13:28:40 -0800

    fix library name for prints matcher
    Review URL: https://codereview.chromium.org//910943002

--- test/prints_matcher_test.dart
+++ test/prints_matcher_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library matcher.prints_matchers_test;
+library unittest.prints_matchers_test;
 
 import 'dart:async';
 
