ed2327e02f9c8aa23e331fa53ed9111544fc2512
Natalie Weizenbaum <nweiz@google.com>
2015-03-03 11:56:38 -0800

    Add a shelf handler for running one-off handlers.
    See #5
    
    R=kevmoo@google.com
    
    Review URL: https://codereview.chromium.org//962363003

---
+++ lib/src/util/one_off_handler.dart
@@ -0,0 +1,47 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library unittest.one_off_handler;
+
+import 'package:path/path.dart' as p;
+import 'package:shelf/shelf.dart' as shelf;
+
+/// A Shelf handler that provides support for one-time handlers.
+///
+/// This is useful for handlers that only expect to be hit once before becoming
+/// invalid and don't need to have a persistent URL.
+class OneOffHandler {
+  /// A map from URL paths to handlers.
+  final _handlers = new Map<String, shelf.Handler>();
+
+  /// The counter of handlers that have been activated.
+  var _counter = 0;
+
+  /// The actual [shelf.Handler] that dispatches requests.
+  shelf.Handler get handler => _onRequest;
+
+  /// Creates a new one-off handler that forwards to [handler].
+  ///
+  /// Returns a string that's the URL path for hitting this handler, relative to
+  /// the URL for the one-off handler itself.
+  ///
+  /// [handler] will be unmounted as soon as it receives a request.
+  String create(shelf.Handler handler) {
+    var path = _counter.toString();
+    _handlers[path] = handler;
+    _counter++;
+    return path;
+  }
+
+  /// Dispatches [request] to the appropriate handler.
+  _onRequest(shelf.Request request) {
+    // Skip the first component because it's always "/".
+    var components = p.url.split(request.url.path).skip(1).toList();
+    if (components.isEmpty) return new shelf.Response.notFound(null);
+
+    var handler = _handlers.remove(components.removeAt(0));
+    if (handler == null) return new shelf.Response.notFound(null);
+    return handler(request);
+  }
+}
---
+++ test/one_off_handler_test.dart
@@ -0,0 +1,84 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:unittest/src/util/one_off_handler.dart';
+import 'package:unittest/unittest.dart';
+
+void main() {
+  var handler;
+  setUp(() => handler = new OneOffHandler());
+
+  _handle(request) => new Future.sync(() => handler.handler(request));
+
+  test("returns a 404 for a root URL", () {
+    var request = new shelf.Request("GET", Uri.parse("http://localhost/"));
+    return _handle(request).then((response) {
+      expect(response.statusCode, equals(404));
+    });
+  });
+
+  test("returns a 404 for an unhandled URL", () {
+    var request = new shelf.Request("GET", Uri.parse("http://localhost/1"));
+    return _handle(request).then((response) {
+      expect(response.statusCode, equals(404));
+    });
+  });
+
+  test("passes a request to a handler only once", () {
+    var path = handler.create(expectAsync((request) {
+      expect(request.method, equals("GET"));
+      return new shelf.Response.ok("good job!");
+    }));
+
+    var request = new shelf.Request("GET", Uri.parse("http://localhost/$path"));
+    return _handle(request).then((response) {
+      expect(response.statusCode, equals(200));
+      expect(response.readAsString(), completion(equals("good job!")));
+
+      request = new shelf.Request("GET", Uri.parse("http://localhost/$path"));
+      return _handle(request);
+    }).then((response) {
+      expect(response.statusCode, equals(404));
+    });
+  });
+
+  test("passes requests to the correct handlers", () {
+    var path1 = handler.create(expectAsync((request) {
+      expect(request.method, equals("GET"));
+      return new shelf.Response.ok("one");
+    }));
+
+    var path2 = handler.create(expectAsync((request) {
+      expect(request.method, equals("GET"));
+      return new shelf.Response.ok("two");
+    }));
+
+    var path3 = handler.create(expectAsync((request) {
+      expect(request.method, equals("GET"));
+      return new shelf.Response.ok("three");
+    }));
+
+    var request = new shelf.Request(
+        "GET", Uri.parse("http://localhost/$path2"));
+    return _handle(request).then((response) {
+      expect(response.statusCode, equals(200));
+      expect(response.readAsString(), completion(equals("two")));
+
+      request = new shelf.Request("GET", Uri.parse("http://localhost/$path1"));
+      return _handle(request);
+    }).then((response) {
+      expect(response.statusCode, equals(200));
+      expect(response.readAsString(), completion(equals("one")));
+
+      request = new shelf.Request("GET", Uri.parse("http://localhost/$path3"));
+      return _handle(request);
+    }).then((response) {
+      expect(response.statusCode, equals(200));
+      expect(response.readAsString(), completion(equals("three")));
+    });
+  });
+}
