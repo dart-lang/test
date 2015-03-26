31e6483b098c420a0f63941c546fa8cc41000cd3
Natalie Weizenbaum <nweiz@google.com>
2015-03-26 16:19:36 -0700

    Release 0.12.0-beta.0.
    This is a beta release since all of the major foundational work is now
    in place.
    
    R=kevmoo@google.com
    
    Review URL: https://codereview.chromium.org//1039033002

--- test/util/string_literal_iterator_test.dart
+++ test/util/string_literal_iterator_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn("vm")
+
 import 'package:analyzer/analyzer.dart';
 import 'package:unittest/unittest.dart';
 import 'package:unittest/src/util/string_literal_iterator.dart';
