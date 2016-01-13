82eacc06dad93a2928acbbaf21dbd663052985d1
Natalie Weizenbaum <nweiz@google.com>
2016-01-12 17:03:33 -0800

    Temporarily re-add a library tag.
    R=kevmoo@google.com
    
    Review URL: https://codereview.chromium.org//1578303003 .

--- test/io.dart
+++ test/io.dart
@@ -2,6 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// TODO(nweiz): Remove this tag when we can get [packageDir] working without it
+// (dart-lang/sdk#24022).
+library test.test.io;
+
 import 'dart:async';
 import 'dart:io';
 
