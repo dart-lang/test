b23550e0345014d56cb4584f6652f28637fb001c
Lex Berezhny <lex@damoti.com>
2017-02-23 17:51:00 -0500

    set iframe size with CSS instead of in code

--- lib/src/runner/browser/static/host.dart
+++ lib/src/runner/browser/static/host.dart
@@ -172,8 +172,6 @@ MultiChannel _connectToServer() {
 StreamChannel _connectToIframe(String url, int id) {
   var iframe = new IFrameElement();
   _iframes[id] = iframe;
-  iframe.width = '800';
-  iframe.height = '600';
   iframe.src = url;
   document.body.children.add(iframe);
 
