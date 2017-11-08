bd7228002c89c813e71637f40e1bde0f9026a996
Natalie Weizenbaum <nweiz@google.com>
2017-11-08 14:01:21 -0800

    Add a neverCalled getter
    Closes #417

--- lib/test.dart
+++ lib/test.dart
@@ -23,6 +23,7 @@ export 'src/frontend/expect.dart' hide formatFailure;
 export 'src/frontend/expect_async.dart';
 export 'src/frontend/future_matchers.dart';
 export 'src/frontend/on_platform.dart';
+export 'src/frontend/never_called.dart';
 export 'src/frontend/prints_matcher.dart';
 export 'src/frontend/skip.dart';
 export 'src/frontend/spawn_hybrid.dart';
