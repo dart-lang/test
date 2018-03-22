b5488f9dfcc2af1f65ab0fd93dea4203b7f9d10c
Natalie Weizenbaum <nweiz@google.com>
2018-03-22 11:42:13 -0700

    Fix formatStackTrace() in dart path/to/test.dart (#792)
    Closes #786

--- lib/src/frontend/format_stack_trace.dart
+++ lib/src/frontend/format_stack_trace.dart
@@ -6,10 +6,17 @@ import 'package:stack_trace/stack_trace.dart';
 
 import '../backend/stack_trace_formatter.dart';
 
+/// The default formatter to use for formatting stack traces.
+///
+/// This is used in situations where the zone-scoped formatter is unavailable,
+/// such as when running via `dart path/to/test.dart'.
+final _defaultFormatter = new StackTraceFormatter();
+
 /// Converts [stackTrace] to a [Chain] according to the current test's
 /// configuration.
 ///
 /// If [verbose] is `true`, this doesn't fold out irrelevant stack frames. It
 /// defaults to the current test's `verbose_trace` configuration.
 Chain formatStackTrace(StackTrace stackTrace, {bool verbose}) =>
-    StackTraceFormatter.current.formatStackTrace(stackTrace, verbose: verbose);
+    (StackTraceFormatter.current ?? _defaultFormatter)
+        .formatStackTrace(stackTrace, verbose: verbose);
