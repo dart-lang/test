c6af9aeb827bbab1b038d7680aee2a82ad1c5654
Jacob MacDonald <jakemac@google.com>
2018-04-06 14:14:34 -0700

    separate out each way of spawning an isolate into their own methods

--- lib/src/runner/vm/platform.dart
+++ lib/src/runner/vm/platform.dart
@@ -54,55 +54,66 @@ class VMPlatform extends PlatformPlugin {
   /// serialized tests over that channel.
   Future<Isolate> _spawnIsolate(String path, SendPort message) async {
     if (_config.suiteDefaults.precompiledPath != null) {
-      path = p.join(_config.suiteDefaults.precompiledPath, path);
-      final resolver = await PackageResolver.loadConfig(new Uri.file(
-          p.join(_config.suiteDefaults.precompiledPath, '.packages')));
-      return await Isolate.spawnUri(
-          p.toUri(p.absolute(path + '.vm_test.dart')), [], message,
-          packageRoot: await resolver.packageRoot,
-          packageConfig: await resolver.packageConfigUri,
-          checked: true);
+      return _spawnPrecompiledIsolate(
+          path, message, _config.suiteDefaults.precompiledPath);
     } else if (_config.pubServeUrl != null) {
-      var url = _config.pubServeUrl.resolveUri(
-          p.toUri(p.relative(path, from: 'test') + '.vm_test.dart'));
-
-      try {
-        return await Isolate.spawnUri(url, [], message, checked: true);
-      } on IsolateSpawnException catch (error) {
-        if (error.message.contains("OS Error: Connection refused") ||
-            error.message.contains("The remote computer refused")) {
-          throw new LoadException(
-              path,
-              "Error getting $url: Connection refused\n"
-              'Make sure "pub serve" is running.');
-        } else if (error.message.contains("404 Not Found")) {
-          throw new LoadException(
-              path,
-              "Error getting $url: 404 Not Found\n"
-              'Make sure "pub serve" is serving the test/ directory.');
-        }
-
-        throw new LoadException(path, error);
-      }
+      return _spawnPubServeIsolate(path, message, _config.pubServeUrl);
     } else {
-      return await dart.runInIsolate('''
-        import "dart:isolate";
-
-        import "package:stream_channel/stream_channel.dart";
-
-        import "package:test/src/runner/plugin/remote_platform_helpers.dart";
-        import "package:test/src/runner/vm/catch_isolate_errors.dart";
-
-        import "${p.toUri(p.absolute(path))}" as test;
-
-        void main(_, SendPort message) {
-          var channel = serializeSuite(() {
-            catchIsolateErrors();
-            return test.main;
-          });
-          new IsolateChannel.connectSend(message).pipe(channel);
-        }
-      ''', message, checked: true);
+      return _spawnDataIsolate(path, message);
     }
+  }
+}
+
+Future<Isolate> _spawnDataIsolate(String path, SendPort message) async {
+  return await dart.runInIsolate('''
+    import "dart:isolate";
+
+    import "package:stream_channel/stream_channel.dart";
+
+    import "package:test/src/runner/plugin/remote_platform_helpers.dart";
+    import "package:test/src/runner/vm/catch_isolate_errors.dart";
+
+    import "${p.toUri(p.absolute(path))}" as test;
+
+    void main(_, SendPort message) {
+      var channel = serializeSuite(() {
+        catchIsolateErrors();
+        return test.main;
+      });
+      new IsolateChannel.connectSend(message).pipe(channel);
+    }
+  ''', message, checked: true);
+}
+
+Future<Isolate> _spawnPrecompiledIsolate(
+    String testPath, SendPort message, String precompiledPath) async {
+  testPath = p.join(precompiledPath, testPath) + '.vm_test.dart';
+  return await Isolate.spawnUri(p.toUri(p.absolute(testPath)), [], message,
+      packageConfig: p.toUri(p.join(precompiledPath, '.packages')),
+      checked: true);
+}
+
+Future<Isolate> _spawnPubServeIsolate(
+    String testPath, SendPort message, Uri pubServeUrl) async {
+  var url = pubServeUrl.resolveUri(
+      p.toUri(p.relative(testPath, from: 'test') + '.vm_test.dart'));
+
+  try {
+    return await Isolate.spawnUri(url, [], message, checked: true);
+  } on IsolateSpawnException catch (error) {
+    if (error.message.contains("OS Error: Connection refused") ||
+        error.message.contains("The remote computer refused")) {
+      throw new LoadException(
+          testPath,
+          "Error getting $url: Connection refused\n"
+          'Make sure "pub serve" is running.');
+    } else if (error.message.contains("404 Not Found")) {
+      throw new LoadException(
+          testPath,
+          "Error getting $url: 404 Not Found\n"
+          'Make sure "pub serve" is serving the test/ directory.');
+    }
+
+    throw new LoadException(testPath, error);
   }
 }
