ab63e724d8baba75536ab43e5ced93b3a82f0167
Keerti Parthasarathy <keertip@google.com>
2018-04-27 13:59:28 -0700

    format code

--- lib/src/runner/configuration/suite.dart
+++ lib/src/runner/configuration/suite.dart
@@ -335,8 +335,7 @@ class SuiteConfiguration {
   /// Any overlapping keys in the maps have their configurations merged in the
   /// returned map.
   Map<T, SuiteConfiguration> _mergeConfigMaps<T>(
-          Map<T, SuiteConfiguration> map1,
-          Map<T, SuiteConfiguration> map2) =>
+          Map<T, SuiteConfiguration> map1, Map<T, SuiteConfiguration> map2) =>
       mergeMaps(map1, map2,
           value: (config1, config2) => config1.merge(config2));
 
