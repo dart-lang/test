83a924465dde3321458b23ea9d7618517c252977
Jacob MacDonald <jakemac@google.com>
2018-05-03 10:06:30 -0700

    try with xvfb run?

--- tool/before_install.sh
+++ tool/before_install.sh
@@ -1,7 +1,3 @@
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 t=0; until (xdpyinfo -display :99 &> /dev/null || test $t -gt 10); do sleep 1; let t=$t+1; done
-
-which google-chrome
-which firefox
-which chrome
