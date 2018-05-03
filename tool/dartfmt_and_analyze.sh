8f786abffe6b2ffbe37d8143208cdc94ac7f12d4
Jacob MacDonald <jakemac@google.com>
2018-05-03 14:26:20 -0700

    add --fatal-infos and --fatal-warnings to dartanalyzer task

--- tool/dartfmt_and_analyze.sh
+++ tool/dartfmt_and_analyze.sh
@@ -4,6 +4,6 @@ pub get || exit $
 
 EXIT_CODE=0
 dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
-dartanalyzer . || EXIT_CODE=$?
+dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
 
 exit $EXIT_CODE
