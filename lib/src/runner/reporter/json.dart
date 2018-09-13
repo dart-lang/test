2ee1acefb0ce80bd2ccc0d3db4ae72767b60d6c8
Jacob MacDonald <jakemac@google.com>
2018-09-13 13:07:29 -0700

    fix json reporter to not assume file uris for frames

--- lib/src/runner/reporter/json.dart
+++ lib/src/runner/reporter/json.dart
@@ -297,7 +297,9 @@ class JsonReporter implements Reporter {
       String suitePath) {
     var frame = entry.trace?.frames?.first;
     var rootFrame = entry.trace?.frames?.firstWhere(
-        (frame) => frame.uri.toFilePath() == p.absolute(suitePath),
+        (frame) =>
+            frame.uri.scheme == 'file' &&
+            frame.uri.toFilePath() == p.absolute(suitePath),
         orElse: () => null);
     if (suiteConfig.jsTrace && runtime.isJS) {
       frame = null;
