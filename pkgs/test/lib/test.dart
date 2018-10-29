170e89ef04da6ab7d4053ba7f4eb2bed451ea3e9
Gary Roumanis <grouma@google.com>
2018-10-29 16:13:24 -0700

    Refactor package:test to better use package:test_api

--- pkgs/test/lib/test.dart
+++ pkgs/test/lib/test.dart
@@ -23,7 +23,7 @@ import 'package:test_api/src/utils.dart'; // ignore: implementation_imports
 // Hide implementations which don't support being run directly.
 // This file is an almost direct copy of import below, but with the global
 // declarer added.
-export 'package:test_api/test.dart'
+export 'package:test_api/test_api.dart'
     hide test, group, setUp, setUpAll, tearDown, tearDownAll;
 
 /// The global declarer.
