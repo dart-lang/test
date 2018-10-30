e64624c986590380cd9ef341f6cd6e726a6c3d25
Gary Roumanis <grouma@google.com>
2018-10-30 10:31:44 -0700

    Update dartfmt and analyze

--- tool/travis.sh
+++ tool/travis.sh
@@ -21,38 +21,43 @@ while (( "$#" )); do
   case $TASK in
   command_0) echo
     echo -e '\033[1mTASK: command_0\033[22m'
-    echo -e 'tool/dartfmt_and_analyze.sh'
-    tool/dartfmt_and_analyze.sh || EXIT_CODE=$?
-    ;;
-  command_1) echo
-    echo -e '\033[1mTASK: command_1\033[22m'
     echo -e 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 0'
     xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 0 || EXIT_CODE=$?
     ;;
-  command_2) echo
-    echo -e '\033[1mTASK: command_2\033[22m'
+  command_1) echo
+    echo -e '\033[1mTASK: command_1\033[22m'
     echo -e 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 1'
     xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 1 || EXIT_CODE=$?
     ;;
-  command_3) echo
-    echo -e '\033[1mTASK: command_3\033[22m'
+  command_2) echo
+    echo -e '\033[1mTASK: command_2\033[22m'
     echo -e 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 2'
     xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 2 || EXIT_CODE=$?
     ;;
-  command_4) echo
-    echo -e '\033[1mTASK: command_4\033[22m'
+  command_3) echo
+    echo -e '\033[1mTASK: command_3\033[22m'
     echo -e 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 3'
     xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 3 || EXIT_CODE=$?
     ;;
-  command_5) echo
-    echo -e '\033[1mTASK: command_5\033[22m'
+  command_4) echo
+    echo -e '\033[1mTASK: command_4\033[22m'
     echo -e 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 4'
     xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 4 || EXIT_CODE=$?
     ;;
-  command_6) echo
-    echo -e '\033[1mTASK: command_6\033[22m'
+  command_5) echo
+    echo -e '\033[1mTASK: command_5\033[22m'
     echo -e 'pub run test --preset travis'
     pub run test --preset travis || EXIT_CODE=$?
+    ;;
+  dartanalyzer) echo
+    echo -e '\033[1mTASK: dartanalyzer\033[22m'
+    echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+    dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
+    ;;
+  dartfmt) echo
+    echo -e '\033[1mTASK: dartfmt\033[22m'
+    echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
     ;;
   *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
     EXIT_CODE=1
