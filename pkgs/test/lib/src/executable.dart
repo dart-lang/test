ae3019de89541193e65b4dbf9ace67fa904401ee
Gary Roumanis <grouma@google.com>
2018-10-31 17:17:25 -0700

    add back copyright

--- pkgs/test/lib/src/executable.dart
+++ pkgs/test/lib/src/executable.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // ignore: implementation_imports
 import 'package:test_core/src/executable.dart' as executable;
 import 'package:test_core/src/runner/hack_register_platform.dart'; // ignore: implementation_imports
