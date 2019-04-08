daa9eb4f31d89db7afab9450508feb1c236eb17d
Kevin Moore <kevmoo@google.com>
2019-04-08 12:10:58 -0700

    travis: Run dartfmt only on dev
    Run analyzer (at a minimum) across all packages at their oldest supported SDK

--- tool/travis.sh
+++ tool/travis.sh
@@ -45,15 +45,30 @@ for PKG in ${PKGS}; do
       echo -e 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 4'
       xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis --total-shards 5 --shard-index 4 || EXIT_CODE=$?
       ;;
-    command_5) echo
-      echo -e '\033[1mTASK: command_5\033[22m'
-      echo -e 'pub run test --preset travis'
-      pub run test --preset travis || EXIT_CODE=$?
-      ;;
-    dartanalyzer) echo
-      echo -e '\033[1mTASK: dartanalyzer\033[22m'
+    dartanalyzer_0) echo
+      echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
       echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
       dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
+      ;;
+    dartanalyzer_1) echo
+      echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
+      echo -e 'dartanalyzer --fatal-warnings .'
+      dartanalyzer --fatal-warnings . || EXIT_CODE=$?
+      ;;
+    dartanalyzer_2) echo
+      echo -e '\033[1mTASK: dartanalyzer_2\033[22m'
+      echo -e 'dartanalyzer --fatal-infos .'
+      dartanalyzer --fatal-infos . || EXIT_CODE=$?
+      ;;
+    dartfmt) echo
+      echo -e '\033[1mTASK: dartfmt\033[22m'
+      echo -e 'dartfmt -n --set-exit-if-changed .'
+      dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+      ;;
+    test) echo
+      echo -e '\033[1mTASK: test\033[22m'
+      echo -e 'pub run test --preset travis'
+      pub run test --preset travis || EXIT_CODE=$?
       ;;
     *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
       EXIT_CODE=1
