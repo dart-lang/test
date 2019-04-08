2bb7c0301f36d7db29fd3f7979f2e3a81ba33833
Jacob MacDonald <jakemac@google.com>
2019-04-08 06:43:06 -0700

    cleanup imports/exports

--- pkgs/test_core/lib/backend.dart
+++ pkgs/test_core/lib/backend.dart
@@ -2,10 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'package:test_api/src/backend/metadata.dart' show Metadata;
-export 'package:test_api/src/backend/platform_selector.dart'
-    show PlatformSelector;
-export 'package:test_api/src/backend/runtime.dart' show Runtime;
-export 'package:test_api/src/backend/suite_platform.dart' show SuitePlatform;
+export 'package:test_api/backend.dart'
+    show Metadata, PlatformSelector, Runtime, SuitePlatform;
 
 export 'src/runner/parse_metadata.dart' show parseMetadata;
