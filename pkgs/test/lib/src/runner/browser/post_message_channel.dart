f5d52b3a2edd603623ab07103cd38da4de303177
Jacob MacDonald <jakemac@google.com>
2020-03-19 09:56:25 -0700

    opt public libraries from package:test into nnbd

--- pkgs/test/lib/src/runner/browser/post_message_channel.dart
+++ pkgs/test/lib/src/runner/browser/post_message_channel.dart
@@ -47,7 +47,8 @@ StreamChannel postMessageChannel() {
   // Send a ready message once we're listening so the host knows it's safe to
   // start sending events.
   // TODO(nweiz): Stop manually adding href here once issue 22554 is fixed.
-  _postParentMessage(jsify({'href': window.location.href, 'ready': true}),
+  _postParentMessage(
+      jsify({'href': window.location.href, 'ready': true}) as Object,
       window.location.origin);
 
   return controller.foreign;
