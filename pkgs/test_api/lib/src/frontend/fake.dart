a5f88f48777bdea875d1aa9a5a9676c65293c43e
Paul Berry <paulberry@google.com>
2020-07-21 20:58:13 +0000

    Address code review comments

--- pkgs/test_api/lib/src/frontend/fake.dart
+++ pkgs/test_api/lib/src/frontend/fake.dart
@@ -2,7 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Extend or mixin this class to mark the implementation as a [Fake].
+/// A stand-in for another object which cannot be used except for specifically
+/// overridden methods.
 ///
 /// A fake has a default behavior for every field and method of throwing
 /// [UnimplementedError]. Fields and methods that are excersized by the code
