1c29a6465f269a23d686657e2ecd892519999560
Jacob MacDonald <jakemac@google.com>
2020-07-22 12:50:21 -0700

    Fix travis (#1306)
    * fix dead code warning
    
    * fix some tests by updating the language version

--- pkgs/test/test/runner/runner_test.dart
+++ pkgs/test/test/runner/runner_test.dart
@@ -731,7 +731,7 @@ final foo = true;''').create();
     test('sound null safety is enabled if the entrypoint opts in explicitly',
         () async {
       await d.file('test.dart', '''
-// @dart=2.9
+// @dart=2.10
 $_testContents
 ''').create();
       var test = await runTest(['test.dart']);
@@ -771,7 +771,7 @@ $_testContents''').create();
         var newPackageConfig = PackageConfig([
           ...currentPackageConfig.packages,
           Package('example', Uri.file('${d.sandbox}/'),
-              languageVersion: LanguageVersion(2, 9),
+              languageVersion: LanguageVersion(2, 10),
               // TODO: https://github.com/dart-lang/package_config/issues/81
               packageUriRoot: Uri.file('${d.sandbox}/')),
         ]);
