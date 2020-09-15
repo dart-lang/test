ffd3e49b1b15b995707677e330461f39567354f5
Nate Bosch <nbosch@google.com>
2020-09-15 16:53:50 -0700

    I wish my analysis server was working...

--- pkgs/test_core/lib/src/platform.dart
+++ pkgs/test_core/lib/src/platform.dart
@@ -1,11 +1,11 @@
-export 'package:test_api/backend.dart' show SuitePlatform, Runtime, runtimes;
+// ignore: deprecated_member_use
+export 'package:test_api/backend.dart' show SuitePlatform, Runtime;
 export 'package:test_core/src/runner/configuration.dart' show Configuration;
 export 'package:test_core/src/runner/environment.dart'
     show PluginEnvironment, Environment;
 export 'package:test_core/src/runner/hack_register_platform.dart'
     show registerPlatformPlugin;
-export 'package:test_core/src/runner/platform.dart'
-    show platforms, PlatformPlugin;
+export 'package:test_core/src/runner/platform.dart' show PlatformPlugin;
 export 'package:test_core/src/runner/plugin/platform_helpers.dart'
     show deserializeSuite;
 export 'package:test_core/src/runner/runner_suite.dart'
