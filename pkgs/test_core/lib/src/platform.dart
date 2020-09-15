ac854e9695ff5592b8c019e5ef401f3630363e85
Nate Bosch <nbosch@google.com>
2020-09-15 16:28:46 -0700

    Add a centralized platform import
    Towards #962
    
    Make it easier to import the necessary things for writing a custom
    platform. This does not expose `executable` so a custom test runner
    would need to import at a minimum this and the executable. The symbols
    exported here are the ones used by `flutter_tools` in
    `flutter_platform.dart` and `test_wrapper.dart`, if other platforms need
    other symbols we may need to expand this some.
    
    The import remains a `src` import since these are not yet stable enough
    to be fully public. Centralizing and adding `show` clauses will make it
    easier to know what is used and for which intent.

---
+++ pkgs/test_core/lib/src/platform.dart
@@ -0,0 +1,11 @@
+export 'package:test_api/backend.dart' show SuitePlatform, Runtime, runtimes;
+export 'package:test_core/src/runner/environment.dart' show PluginEnvironment;
+export 'package:test_core/src/runner/hack_register_platform.dart'
+    show registerPlatformPlugin;
+export 'package:test_core/src/runner/platform.dart'
+    show platforms, PlatformPlugin;
+export 'package:test_core/src/runner/plugin/platform_helpers.dart'
+    show deserializeSuite;
+export 'package:test_core/src/runner/runner_suite.dart'
+    show RunnerSuite, RunnerSuiteController;
+export 'package:test_core/src/runner/suite.dart' show SuiteConfiguration;
