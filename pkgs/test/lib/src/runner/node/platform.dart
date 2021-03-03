33a04ab80106b5b6ad968306d1ccb7a04e65cfd5
Jacob MacDonald <jakemac@google.com>
2021-03-03 13:15:00 -0800

    remove cast

--- pkgs/test/lib/src/runner/node/platform.dart
+++ pkgs/test/lib/src/runner/node/platform.dart
@@ -87,8 +87,8 @@ class NodePlatform extends PlatformPlugin
   Future<RunnerSuite> load(String path, SuitePlatform platform,
       SuiteConfiguration suiteConfig, Object message) async {
     var pair = await _loadChannel(path, platform.runtime, suiteConfig);
-    var controller = deserializeSuite(path, platform, suiteConfig,
-        PluginEnvironment(), pair.first.cast(), message);
+    var controller = deserializeSuite(
+        path, platform, suiteConfig, PluginEnvironment(), pair.first, message);
 
     controller.channel('test.node.mapper').sink.add(pair.last?.serialize());
 
