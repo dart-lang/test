aac20edaea53642345f68eb73b73d2cd8c40f5db
Jacob MacDonald <jakemac@google.com>
2021-04-22 13:53:46 -0700

    fix mixed mode tests and add test

--- integration_tests/nnbd_opted_out/test/spawn_hybrid_code_test.dart
+++ integration_tests/nnbd_opted_out/test/spawn_hybrid_code_test.dart
@@ -8,28 +8,21 @@ void main() {
   group('spawnHybridCode', () {
     test('uses the current package language version by default', () async {
       final channel = spawnHybridCode(_hybridMain);
-      expect(await channel.stream.single, equals(false));
+      expect(await channel.stream.single, equals(true));
     });
 
     test('can set the language version with a marker', () async {
       final channel = spawnHybridCode('// @dart=2.12\n$_hybridMain');
-      expect(await channel.stream.single, equals(true));
+      expect(await channel.stream.single, equals(false));
     });
   });
 }
 
 const _hybridMain = '''
-bool runningWithNullSafety() {
-  try {
-    null as String;
-    return false;
-  } catch (_) {
-    return true;
-  }
-}
+final isOptedOut = <int?>[] is List<int>;
 
 void hybridMain(dynamic channel) async {
-  channel.sink.add(runningWithNullSafety());
+  channel.sink.add(isOptedOut);
   channel.sink.close();
 }
 ''';
