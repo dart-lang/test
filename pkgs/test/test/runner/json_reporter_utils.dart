34eff21d43842d636eb7bf382fd48284a54bab17
Nate Bosch <nbosch@google.com>
2021-08-12 11:48:11 -0700

    Undo format

--- pkgs/test/test/runner/json_reporter_utils.dart
+++ pkgs/test/test/runner/json_reporter_utils.dart
@@ -26,8 +26,9 @@ Future<void> expectJsonReport(
       equals(expected.fold<int>(3, (int a, m) => a + m.length)),
       reason: 'Expected $outputLines to match $expected.');
 
-  dynamic decodeLine(String l) =>
-      jsonDecode(l)..remove('time')..remove('stackTrace');
+  dynamic decodeLine(String l) => jsonDecode(l)
+    ..remove('time')
+    ..remove('stackTrace');
 
   // Should contain all suites message.
   expect(outputLines.map(decodeLine), containsAll([allSuitesJson()]));
