884e2184f6b8e7740831a9d47b01d8f11b95a8b0
Jake Macdonald <jakemac@google.com>
2021-10-21 11:08:04 -0700

    fix lint

--- pkgs/test/test/runner/precompiled_test.dart
+++ pkgs/test/test/runner/precompiled_test.dart
@@ -13,7 +13,6 @@ import 'package:path/path.dart' as p;
 import 'package:test_descriptor/test_descriptor.dart' as d;
 import 'package:test_process/test_process.dart';
 
-import 'package:test_core/src/util/io.dart';
 import 'package:test/test.dart';
 import 'package:test/src/util/package_map.dart';
 
