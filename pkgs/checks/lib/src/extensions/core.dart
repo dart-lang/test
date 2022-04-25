bce8308e9d6f0e2421678293ed63ea550f1a9fb0
Nate Bosch <nbosch@google.com>
2022-04-25 13:10:05 -0700

    Shorter equals description

--- pkgs/checks/lib/src/extensions/core.dart
+++ pkgs/checks/lib/src/extensions/core.dart
@@ -80,9 +80,7 @@ extension EqualityChecks<T> on Check<T> {
   void equals(T other) {
     context.expect(() => ['equals ${literal(other)}'], (actual) {
       if (actual == other) return null;
-      return Rejection(
-          actual: literal(actual),
-          which: ['is not equal according to `operator ==`']);
+      return Rejection(actual: literal(actual), which: ['are not equal']);
     });
   }
 
