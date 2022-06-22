6d656f60f2c486efce5f4f22ff00dbb037a825f1
Kevin Moore <kevmoo@google.com>
2022-06-22 13:38:54 -0700

    checks: Add some TODOs around handling String in `literal` helper

--- pkgs/checks/lib/src/describe.dart
+++ pkgs/checks/lib/src/describe.dart
@@ -1,6 +1,8 @@
 String literal(Object? o) {
   if (o == null || o is num || o is bool) return '<$o>';
   // TODO Truncate long strings?
+  // TODO: handle strings with embedded `'`
+  // TODO: special handling of multi-line strings?
   if (o is String) return "'$o'";
   // TODO Truncate long collections?
   return '$o';
