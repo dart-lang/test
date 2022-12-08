5fb821e2318261308072a48f034828af464720f5
Jake Macdonald <jakemac@google.com>
2022-12-08 20:00:21 +0000

    move legacy tests all under a new legacy_tests directory

--- pkgs/test/test/runner/runner_test.dart
+++ pkgs/test/test/runner/runner_test.dart
@@ -4,13 +4,9 @@
 
 @TestOn('vm')
 
-import 'dart:convert';
 import 'dart:io';
-import 'dart:isolate';
 import 'dart:math' as math;
 
-import 'package:package_config/package_config.dart';
-import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 import 'package:test_core/src/util/exit_codes.dart' as exit_codes;
 import 'package:test_descriptor/test_descriptor.dart' as d;
@@ -751,111 +747,6 @@ void main(List<String> args) async {
     'windows': Skip('https://github.com/dart-lang/test/issues/1615')
   });
 
-  group('nnbd', () {
-    final testContents = '''
-import 'package:test/test.dart';
-import 'opted_out.dart';
-
-void main() {
-  test("success", () {
-    expect(foo, true);
-  });
-}''';
-
-    setUp(() async {
-      await d.file('opted_out.dart', '''
-// @dart=2.8
-final foo = true;''').create();
-    });
-
-    test('sound null safety is enabled if the entrypoint opts in explicitly',
-        () async {
-      await d.file('test.dart', '''
-// @dart=2.12
-$testContents
-''').create();
-      var test = await runTest(['test.dart']);
-
-      expect(
-          test.stdout,
-          emitsThrough(contains(
-              'Error: A library can\'t opt out of null safety by default, '
-              'when using sound null safety.')));
-      await test.shouldExit(1);
-    });
-
-    test('sound null safety is disabled if the entrypoint opts out explicitly',
-        () async {
-      await d.file('test.dart', '''
-// @dart=2.8
-$testContents''').create();
-      var test = await runTest(['test.dart']);
-
-      expect(test.stdout, emitsThrough(contains('+1: All tests passed!')));
-      await test.shouldExit(0);
-    });
-
-    group('defaults', () {
-      late PackageConfig currentPackageConfig;
-
-      setUpAll(() async {
-        currentPackageConfig =
-            await loadPackageConfigUri((await Isolate.packageConfig)!);
-      });
-
-      setUp(() async {
-        await d.file('test.dart', testContents).create();
-      });
-
-      test('sound null safety is enabled if the package is opted in', () async {
-        var newPackageConfig = PackageConfig([
-          ...currentPackageConfig.packages,
-          Package('example', Uri.file('${d.sandbox}/'),
-              languageVersion: LanguageVersion(2, 12),
-              // TODO: https://github.com/dart-lang/package_config/issues/81
-              packageUriRoot: Uri.file('${d.sandbox}/')),
-        ]);
-
-        await d
-            .file('package_config.json',
-                jsonEncode(PackageConfig.toJson(newPackageConfig)))
-            .create();
-
-        var test = await runTest(['test.dart'],
-            packageConfig: p.join(d.sandbox, 'package_config.json'));
-
-        expect(
-            test.stdout,
-            emitsThrough(contains(
-                'Error: A library can\'t opt out of null safety by default, '
-                'when using sound null safety.')));
-        await test.shouldExit(1);
-      });
-
-      test('sound null safety is disabled if the package is opted out',
-          () async {
-        var newPackageConfig = PackageConfig([
-          ...currentPackageConfig.packages,
-          Package('example', Uri.file('${d.sandbox}/'),
-              languageVersion: LanguageVersion(2, 8),
-              // TODO: https://github.com/dart-lang/package_config/issues/81
-              packageUriRoot: Uri.file('${d.sandbox}/')),
-        ]);
-
-        await d
-            .file('package_config.json',
-                jsonEncode(PackageConfig.toJson(newPackageConfig)))
-            .create();
-
-        var test = await runTest(['test.dart'],
-            packageConfig: p.join(d.sandbox, 'package_config.json'));
-
-        expect(test.stdout, emitsThrough(contains('+1: All tests passed!')));
-        await test.shouldExit(0);
-      });
-    });
-  });
-
   group('language experiments', () {
     group('are inherited from the executable arguments', () {
       setUp(() async {
@@ -887,7 +778,7 @@ void main() {
           await expectLater(
               test.stdout, emitsThrough(contains('+1: All tests passed!')));
           await test.shouldExit(0);
-        });
+        }, skip: 'https://github.com/dart-lang/test/issues/1813');
       }
     });
   });
