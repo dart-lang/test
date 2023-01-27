a8178c3fe6efe6555163206b071551c56b6f651c
Nate Bosch <nbosch@google.com>
2023-01-27 02:38:34 +0000

    Bad merge

--- pkgs/checks/test/extensions/async_test.dart
+++ pkgs/checks/test/extensions/async_test.dart
@@ -29,7 +29,7 @@ void main() {
         await checkThat(it<Future<void>>()..completes())
             .asyncDescription
             .which(it()..deepEquals(['  completes to a value']));
-        await checkThat(it<Future<void>>()..completes().which(it()..equals(42)))
+        await checkThat(it<Future<int>>()..completes().which(it()..equals(42)))
             .asyncDescription
             .which(it()
               ..deepEquals([
