f35000569fe0d15d45c3b37e9f7fec7411eb9bdc
Nate Bosch <nbosch@google.com>
2023-01-31 00:03:58 +0000

    Renames in example

--- pkgs/checks/example/example.dart
+++ pkgs/checks/example/example.dart
@@ -8,14 +8,14 @@ import 'package:test/scaffolding.dart';
 void main() {
   test('sample test', () {
     final someValue = 5;
-    checkThat(someValue).equals(5);
+    check(someValue).equals(5);
 
     final someList = [1, 2, 3, 4, 5];
-    checkThat(someList).deepEquals([1, 2, 3, 4, 5]);
+    check(someList).deepEquals([1, 2, 3, 4, 5]);
 
     final someString = 'abcdefghijklmnopqrstuvwxyz';
 
-    checkThat(someString)
+    check(someString)
       ..startsWith('a')
       ..endsWith('z')
       ..contains('lmno');
