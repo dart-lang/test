c6fab49bf8b81cf5795131d0c33d53e5a225dc17
Nate Bosch <nbosch@google.com>
2023-03-16 20:02:55 +0000

    Restore an in use src/ import

---
+++ pkgs/test_api/lib/src/expect/async_matcher.dart
@@ -0,0 +1,6 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+@Deprecated('Import from package:matcher if use cannot be avoided')
+export 'package:matcher/src/expect/async_matcher.dart';
