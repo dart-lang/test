86c5ccee5ecd071fa25a1cf0bcbb5e75c9a3dfeb
Nate Bosch <nbosch@google.com>
2023-03-16 20:29:32 +0000

    Don't deprecate the export
    Some internal targets make this a hard failure :(

--- pkgs/test_api/lib/src/expect/async_matcher.dart
+++ pkgs/test_api/lib/src/expect/async_matcher.dart
@@ -2,5 +2,4 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@Deprecated('Import from package:matcher if use cannot be avoided')
 export 'package:matcher/src/expect/async_matcher.dart';
