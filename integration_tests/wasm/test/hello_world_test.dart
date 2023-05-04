2bd49860ab9d5e853ccd2ca49922a06555cc72f8
Jackson Gardner <jacksongardner@google.com>
2023-05-04 14:39:36 -0700

    Don't use unnamed library.

--- integration_tests/wasm/test/hello_world_test.dart
+++ integration_tests/wasm/test/hello_world_test.dart
@@ -1,10 +1,9 @@
 // Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
 @TestOn('wasm')
 @Retry(2)
-library;
-
 import 'package:test/test.dart';
 
 void main() {
