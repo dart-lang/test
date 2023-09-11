64494959ecdb2e777711487a1fb86555c0c20570
Jacob MacDonald <jakemac@google.com>
2023-09-11 10:17:15 -0700

    skip failing wasm tests (#2091)
    See https://github.com/dart-lang/sdk/issues/53493

--- integration_tests/wasm/test/hello_world_test.dart
+++ integration_tests/wasm/test/hello_world_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@Skip('https://github.com/dart-lang/sdk/issues/53493')
 @TestOn('wasm')
 // This retry is a regression test for https://github.com/dart-lang/test/issues/2006
 @Retry(2)
