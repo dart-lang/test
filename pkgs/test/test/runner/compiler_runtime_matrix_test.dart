a920a6e5357beb0f4a157fb3827ea6a7ecf70d7e
Jake Macdonald <jakemac@google.com>
2023-12-05 21:36:53 +0000

    only skip if compiler is exe also

--- pkgs/test/test/runner/compiler_runtime_matrix_test.dart
+++ pkgs/test/test/runner/compiler_runtime_matrix_test.dart
@@ -109,7 +109,7 @@ void main() {
             expect(test.stderr, emits('world'));
             await test.shouldExit(0);
           },
-              skip: Platform.isWindows
+              skip: Platform.isWindows && compiler == Compiler.exe
                   ? 'https://github.com/dart-lang/test/issues/2150'
                   : null);
         }
