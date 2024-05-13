5cc9bc18b80d45947b36b7c01492759c25c3ad2d
Kenzie Schmoll <kenzieschmoll@google.com>
2024-05-13 10:54:01 -0700

    formatting

--- pkgs/test_core/lib/src/util/prefix.dart
+++ pkgs/test_core/lib/src/util/prefix.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// The prefix for the test library import in generated test runner file.
-/// 
+///
 /// DO NOT CHANGE THE VALUE OF THIS STRING. Some tools depend on logic that
 /// searches for a prefix named 'test' to find the Dart library being tested.
 const testLibraryImportPrefix = 'test';
