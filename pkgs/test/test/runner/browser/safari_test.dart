9a2d155b648ecf3c2e979233ee5e94d199a7d6dd
Nate Bosch <nbosch@google.com>
2024-09-11 14:07:54 -0700

    Run package:test tests on mac (#2280)
    This will exercise things like launching browsers (where they are
    currently working on CI).
    
    Add `osx` to the sharded `package:test` tests alongside `windows`. These
    exercise the majority of OS specific behavior.
    
    Skip existing safari tests due to a bug running tests in non-interactive
    environments. (#1253)
    
    Skip existing firefox tests due to a changed executable path. (Planned
    fix in #2276)
    
    Refactor a ternary chain to an if/else chain to add the extra
    conditions for firefox and safari.

--- pkgs/test/test/runner/browser/safari_test.dart
+++ pkgs/test/test/runner/browser/safari_test.dart
@@ -18,7 +18,8 @@ import 'code_server.dart';
 void main() {
   setUpAll(precompileTestExecutable);
 
-  test('starts Safari with the given URL', () async {
+  test('starts Safari with the given URL',
+      skip: 'https://github.com/dart-lang/test/issues/1253', () async {
     var server = await CodeServer.start();
 
     server.handleJavaScript('''
@@ -54,7 +55,8 @@ webSocket.addEventListener("open", function() {
             startsWith('Failed to run Safari: $noSuchFileMessage'))));
   });
 
-  test('can run successful tests', () async {
+  test('can run successful tests',
+      skip: 'https://github.com/dart-lang/test/issues/1253', () async {
     await d.file('test.dart', '''
 import 'package:test/test.dart';
 
