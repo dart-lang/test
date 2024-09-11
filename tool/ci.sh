c1614bea41f0b7045a4ef3abdeec92df1f800cf2
Jake Macdonald <jakemac@google.com>
2024-09-11 21:10:03 +0000

    run test_api tests on all platforms and compilers

--- tool/ci.sh
+++ tool/ci.sh
@@ -116,8 +116,8 @@ for PKG in ${PKGS}; do
         dart test --preset travis --total-shards 5 --shard-index 4 || EXIT_CODE=$?
         ;;
       command_11)
-        echo 'dart test --preset travis -x browser'
-        dart test --preset travis -x browser || EXIT_CODE=$?
+        echo 'dart test -p vm,chrome -c dart2js,dart2wasm,exe,kernel,source'
+        dart test -p vm,chrome -c dart2js,dart2wasm,exe,kernel,source || EXIT_CODE=$?
         ;;
       format)
         echo 'dart format --output=none --set-exit-if-changed .'
