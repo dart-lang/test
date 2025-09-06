07742be6b3db9660447a8a8f2e0818cb884b0b7a
Nate Bosch <nbosch@google.com>
2025-09-06 00:10:33 +0000

    Remove extra version info
    Follow up to #2538
    
    This is only relevant when using the test runner as a git or path
    dependency. In these cases the version will already end in `-WIP` with
    out current development practices and in the places where it may come up
    the users don't need help understanding where the code is coming from.

--- pkgs/test_core/lib/src/runner/version.dart
+++ pkgs/test_core/lib/src/runner/version.dart
@@ -58,36 +58,8 @@ String? _readPubspecLock() {
   if (packages is! Map) return null;
   var package = packages['test'];
   if (package is! Map) return null;
-
   var source = package['source'];
   if (source is! String) return null;
-
-  switch (source) {
-    case 'hosted':
-      var version = package['version'];
-      return (version is String) ? version : null;
-
-    case 'git':
-      var version = package['version'];
-      if (version is! String) return null;
-      var description = package['description'];
-      if (description is! Map) return null;
-      var ref = description['resolved-ref'];
-      if (ref is! String) return null;
-
-      return '$version (${ref.substring(0, 7)})';
-
-    case 'path':
-      var version = package['version'];
-      if (version is! String) return null;
-      var description = package['description'];
-      if (description is! Map) return null;
-      var path = description['path'];
-      if (path is! String) return null;
-
-      return '$version (from $path)';
-
-    default:
-      return null;
-  }
+  var version = package['version'];
+  return (version is String) ? version : null;
 }
