4b45ecd47a06d60ed602096b65c246d45d73bcd4
Nate Bosch <nbosch@google.com>
2026-02-11 20:40:31 +0000

    Missed save after removing import

--- pkgs/checks/test/pretty_print_test.dart
+++ pkgs/checks/test/pretty_print_test.dart
@@ -5,7 +5,6 @@
 import 'package:checks/checks.dart';
 import 'package:checks/context.dart';
 import 'package:test/scaffolding.dart';
-import 'package:test_api/hooks.dart';
 
 void main() {
   group('literal', () {
