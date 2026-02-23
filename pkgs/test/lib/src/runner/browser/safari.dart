b7f87f97498c5e27cc56c6f746f5f1b08b687bbb
Kevin Moore <kevmoo@google.com>
2026-02-23 11:51:28 -0800

    Move to Safari drive to try to make safari testing less flaky

--- pkgs/test/lib/src/runner/browser/safari.dart
+++ pkgs/test/lib/src/runner/browser/safari.dart
@@ -2,11 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:path/path.dart' as p;
 import 'package:test_api/src/backend/runtime.dart'; // ignore: implementation_imports
 import 'package:test_core/src/util/io.dart'; // ignore: implementation_imports
 
@@ -26,29 +24,119 @@ class Safari extends Browser {
         () => _startBrowser(url, settings ?? defaultSettings[Runtime.safari]!),
       );
 
+  static const _safariDriverInstructions = '''
+A test failed to connect to Safari.
+Safari requires 'safaridriver' to be authenticated to allow remote automation.
+If you are running this on CI or a new Mac, please ensure the following commands are run:
+
+# Enable the Develop menu
+defaults write com.apple.Safari IncludeDevelopMenu YES
+
+# Enable Allow Remote Automation
+defaults write com.apple.Safari AllowRemoteAutomation 1
+
+# Authenticate safaridriver for the current session
+sudo safaridriver --enable
+''';
+
   /// Starts a new instance of Safari open to the given [url], which may be a
   /// [Uri] or a [String].
   static Future<Process> _startBrowser(
     Uri url,
     ExecutableSettings settings,
   ) async {
-    var dir = createTempDir();
+    var executable = settings.executable;
+    if (!File(executable).existsSync()) {
+      return await Process.start(executable, []);
+    }
 
-    // Safari will only open files (not general URLs) via the command-line
-    // API, so we create a dummy file to redirect it to the page we actually
-    // want it to load.
-    var redirect = p.join(dir, 'redirect.html');
-    File(redirect).writeAsStringSync(
-      '<script>location = ${jsonEncode(url.toString())}</script>',
-    );
+    var port = await getUnusedPort<int>((p) async => p);
+    Process process;
+    try {
+      process = await Process.start('safaridriver', [
+        '--port',
+        port.toString(),
+      ]);
+    } catch (_) {
+      stderr.writeln('safaridriver failed to start.');
+      stderr.writeln(_safariDriverInstructions);
+      rethrow;
+    }
 
-    var process = await Process.start(
-      settings.executable,
-      settings.arguments.toList()..add(redirect),
-    );
+    () async {
+      var sessionCreated = false;
+      try {
+        var httpClient = HttpClient();
+        var response = await _sendRequest(
+          httpClient,
+          port,
+          '/session',
+          '{"capabilities": {"alwaysMatch": {"browserName": "Safari"}}}',
+        );
+        var json = jsonDecode(response) as Map<String, dynamic>;
 
-    unawaited(process.exitCode.then((_) => Directory(dir).deleteWithRetry()));
+        var value = json['value'] as Map<String, dynamic>?;
+        if (value != null && value.containsKey('error')) {
+          stderr.writeln('safaridriver failed to create a session.');
+          stderr.writeln(value['message']);
+          stderr.writeln(_safariDriverInstructions);
+          process.kill();
+          return;
+        }
+
+        var sessionId = value!['sessionId'] as String;
+        sessionCreated = true;
+
+        await _sendRequest(
+          httpClient,
+          port,
+          '/session/$sessionId/url',
+          '{"url": "${url.toString()}"}',
+        );
+        httpClient.close();
+      } catch (e) {
+        if (!sessionCreated) {
+          var isKilled = false;
+          try {
+            var exitCode = await process.exitCode.timeout(
+              const Duration(milliseconds: 250),
+            );
+            if (exitCode < 0) isKilled = true;
+          } catch (_) {}
+
+          if (!isKilled) {
+            stderr.writeln('Exception while connecting to safaridriver: $e');
+            stderr.writeln(_safariDriverInstructions);
+          }
+        }
+        process.kill();
+      }
+    }();
 
     return process;
+  }
+
+  static Future<String> _sendRequest(
+    HttpClient client,
+    int port,
+    String path,
+    String body,
+  ) async {
+    var retries = 0;
+    while (true) {
+      try {
+        var request = await client.post('127.0.0.1', port, path);
+        request.headers.contentType = ContentType.json;
+        var encodedBody = utf8.encode(body);
+        request.headers.contentLength = encodedBody.length;
+        request.add(encodedBody);
+        var response = await request.close();
+        return await response.transform(utf8.decoder).join();
+      } catch (e) {
+        if (retries > 5) rethrow;
+        retries++;
+        await Future<void>.delayed(const Duration(milliseconds: 200));
+      }
+    }
   }
 }
--- pkgs/test/test/runner/browser/safari_test.dart
+++ pkgs/test/test/runner/browser/safari_test.dart
@@ -18,26 +18,22 @@ import 'code_server.dart';
 void main() {
   setUpAll(precompileTestExecutable);
 
-  test(
-    'starts Safari with the given URL',
-    skip: 'https://github.com/dart-lang/test/issues/1253',
-    () async {
-      var server = await CodeServer.start();
+  test('starts Safari with the given URL', () async {
+    var server = await CodeServer.start();
 
-      server.handleJavaScript('''
+    server.handleJavaScript('''
 var webSocket = new WebSocket(window.location.href.replace("http://", "ws://"));
 webSocket.addEventListener("open", function() {
   webSocket.send("loaded!");
 });
 ''');
-      var webSocket = server.handleWebSocket();
+    var webSocket = server.handleWebSocket();
 
-      var safari = Safari(server.url);
-      addTearDown(() => safari.close());
+    var safari = Safari(server.url);
+    addTearDown(() => safari.close());
 
-      expect(await (await webSocket).stream.first, equals('loaded!'));
-    },
-  );
+    expect(await (await webSocket).stream.first, equals('loaded!'));
+  });
 
   test("a process can be killed synchronously after it's started", () async {
     var server = await CodeServer.start();
@@ -65,11 +61,8 @@ webSocket.addEventListener("open", function() {
     );
   });
 
-  test(
-    'can run successful tests',
-    skip: 'https://github.com/dart-lang/test/issues/1253',
-    () async {
-      await d.file('test.dart', '''
+  test('can run successful tests', () async {
+    await d.file('test.dart', '''
 import 'package:test/test.dart';
 
 void main() {
@@ -77,11 +70,10 @@ void main() {
 }
 ''').create();
 
-      var test = await runTest(['-p', 'safari', 'test.dart']);
-      expect(test.stdout, emitsThrough(contains('+1: All tests passed!')));
-      await test.shouldExit(0);
-    },
-  );
+    var test = await runTest(['-p', 'safari', 'test.dart']);
+    expect(test.stdout, emitsThrough(contains('+1: All tests passed!')));
+    await test.shouldExit(0);
+  });
 
   test('can run failing tests', () async {
     await d.file('test.dart', '''
