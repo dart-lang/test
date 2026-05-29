493bc0af6702b62d049b3ccb74a386f31cbb2a7a
Nate Bosch <nbosch@google.com>
2026-05-29 00:02:38 +0000

    Use Target and TargetKind

--- pkgs/checks_codegen/lib/src/annotation.dart
+++ pkgs/checks_codegen/lib/src/annotation.dart
@@ -2,10 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:meta/meta_meta.dart';
+
 /// Annotation specifying types to generate Subject extensions with `has`
 /// getters for fields.
 ///
-/// Annotate an import to the `.checks.dart` library.
+/// Annotate an import or export of the `.checks.dart` library.
+@Target({
+  // ignore: deprecated_member_use TODO use importDirective
+  TargetKind.directive,
+  TargetKind.exportDirective,
+})
 final class CheckExtensions {
   final List<Type> types;
   const CheckExtensions(this.types);
