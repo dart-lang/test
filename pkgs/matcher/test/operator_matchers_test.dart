503d85cb6dd3ccebecd94b5ce6bffc34ac7b33c2
Nate Bosch <nbosch@google.com>
2026-06-01 20:53:26 +0000

    Fix import shows. Move imports to same-package where feasible

--- pkgs/matcher/test/operator_matchers_test.dart
+++ pkgs/matcher/test/operator_matchers_test.dart
@@ -2,8 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:matcher/expect.dart' show expect, throwsA, throwsArgumentError;
 import 'package:matcher/matcher.dart';
-import 'package:test/test.dart' show expect, test, throwsArgumentError, throwsA;
+import 'package:test/test.dart' show test;
 
 import 'test_utils.dart';
 
