23dad27e7e5ac5fe5910fa3bd64603977eecdb80
Nate Bosch <nbosch@google.com>
2026-06-26 17:17:54 +0000

    Add a library doc comment

--- pkgs/test_core/lib/src/runner/reporter/direct.dart
+++ pkgs/test_core/lib/src/runner/reporter/direct.dart
@@ -2,4 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// A utility to pick a platform safe default reporter for cases where a test
+/// suite is run directly instead of through the test runner.
+library;
+
 export 'direct_stub.dart' if (dart.library.io) 'direct_io.dart';
