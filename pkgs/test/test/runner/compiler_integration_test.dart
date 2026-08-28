5b8c291d31609fa5ece3040c28f4ff560459a34f
Nate Bosch <nbosch@google.com>
2026-08-28 19:37:56 +0000

    Update chrome test with 25s delay to reproduce socket hang with chrome

--- pkgs/test/test/runner/compiler_integration_test.dart
+++ pkgs/test/test/runner/compiler_integration_test.dart
@@ -18,57 +18,41 @@ void main() {
 
   group('multiplatform runner', () {
     test('can run multiple vm exe suites concurrently', () async {
-      await d.file('slow_test.dart', '''
-import 'package:analyzer/dart/analysis/analysis_context.dart';
+      await d.file('test0.dart', '''
 import 'package:test/test.dart';
 
 void main() {
-  test('slow to compile, quick to run', () {
-    expect(AnalysisContext, isNotNull);
-  });
-}
-''').create();
-
-      final fastNames = [
-        'fast0.dart',
-        'fast1.dart',
-        'fast2.dart',
-        'fast3.dart',
-      ];
-      for (var name in fastNames) {
-        await d.file(name, '''
-import 'package:test/test.dart';
-
-void main() {
-  test('quick to compile, long to run', () async {
-    await Future<void>.delayed(const Duration(seconds: 25));
+  test('vm exe test 0', () {
     expect(1, equals(1));
   });
 }
 ''').create();
-      }
+
+      await d.file('test1.dart', '''
+import 'package:test/test.dart';
+
+void main() {
+  test('vm exe test 1', () {
+    expect(1, equals(1));
+  });
+}
+''').create();
 
       var test = await runTest(
-        [
-          'slow_test.dart',
-          ...fastNames,
-          '-p',
-          'vm',
-          '-c',
-          'exe',
-          '--suite-load-timeout=15s',
-        ],
-        concurrency: 5,
+        ['test0.dart', 'test1.dart', '-p', 'vm', '-c', 'exe'],
+        concurrency: 2,
         forwardStdio: true,
         reporter: 'expanded',
       );
 
       expect(test.stdout, emitsThrough(contains('All tests passed!')));
       await test.shouldExit(0);
-    }, timeout: const Timeout(Duration(minutes: 2)));
+    });
 
-    test('can run vm exe and chrome suites concurrently', () async {
-      await d.file('vm_test.dart', '''
+    test(
+      'can run vm exe and chrome suites concurrently',
+      () async {
+        await d.file('vm_test.dart', '''
 @TestOn('vm')
 library;
 
@@ -82,36 +66,41 @@ void main() {
 }
 ''').create();
 
-      await d.file('chrome_test.dart', '''
+        await d.file('chrome_test.dart', '''
 @TestOn('browser')
 library;
 
 import 'package:test/test.dart';
 
 void main() {
-  test('chrome test', () {
+  test('chrome test', () async {
+    await Future<void>.delayed(const Duration(seconds: 25));
     expect(1, equals(1));
   });
 }
 ''').create();
 
-      var test = await runTest(
-        [
-          'vm_test.dart',
-          'chrome_test.dart',
-          '-p',
-          'vm,chrome',
-          '-c',
-          'exe,dart2js',
-        ],
-        concurrency: 4,
-        forwardStdio: true,
-        reporter: 'expanded',
-      );
+        var test = await runTest(
+          [
+            'vm_test.dart',
+            'chrome_test.dart',
+            '-p',
+            'vm,chrome',
+            '-c',
+            'exe,dart2js',
+            '--suite-load-timeout=10s',
+          ],
+          concurrency: 4,
+          forwardStdio: true,
+          reporter: 'expanded',
+        );
 
-      expect(test.stdout, emitsThrough(contains('All tests passed!')));
-      await test.shouldExit(0);
-    }, tags: 'chrome');
+        expect(test.stdout, emitsThrough(contains('All tests passed!')));
+        await test.shouldExit(0);
+      },
+      tags: 'chrome',
+      timeout: const Timeout(Duration(minutes: 2)),
+    );
 
     test(
       'can run vm cli and chrome suites concurrently',
