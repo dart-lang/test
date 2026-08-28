3fed7646d516f0e47300234f74c2eb5ecf70c9d9
Nate Bosch <nbosch@google.com>
2026-08-28 06:13:00 +0000

    Increase test delay in concurrent suites test to ensure active process overlap

--- pkgs/test/test/runner/compiler_runtime_matrix_test.dart
+++ pkgs/test/test/runner/compiler_runtime_matrix_test.dart
@@ -258,7 +258,7 @@ final _delayedGoodTest = '''
 
   void main() {
     test("success", () async {
-      await Future<void>.delayed(const Duration(seconds: 1));
+      await Future<void>.delayed(const Duration(seconds: 8));
     });
   }
 ''';
