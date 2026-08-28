e8ca2e539562406a3c02a52060c2965d366009ee
Nate Bosch <nbosch@google.com>
2026-08-28 17:55:25 +0000

    Use staggered suite sizes and 12 suites in compiler_integration_test.dart

--- pkgs/test/test/runner/compiler_integration_test.dart
+++ pkgs/test/test/runner/compiler_integration_test.dart
@@ -11,15 +11,21 @@ import 'package:test_descriptor/test_descriptor.dart' as d;
 
 import '../io.dart';
 
-final _test = '''
+String _testSuite(int numTests) {
+  final sb = StringBuffer('''
 import 'package:test/test.dart';
 
 void main() {
-  test("success", () async {
-    await Future<void>.delayed(const Duration(seconds: 15));
+  test('primary', () async {
+    await Future<void>.delayed(const Duration(seconds: 2));
   });
+''');
+  for (var i = 0; i < numTests; i++) {
+    sb.writeln("  test('dummy $i', () { expect($i, $i); });");
+  }
+  sb.writeln('}');
+  return sb.toString();
 }
-''';
 
 void main() {
   setUpAll(() async {
@@ -28,52 +34,55 @@ void main() {
 
   group('multiplatform runner', () {
     test('can run multiple vm exe suites concurrently', () async {
-      for (var i = 1; i <= 4; i++) {
-        await d.file('test$i.dart', _test).create();
+      final counts = [1, 50, 100, 200, 1, 50, 100, 200, 1, 50, 100, 200];
+      for (var i = 0; i < counts.length; i++) {
+        await d.file('test$i.dart', _testSuite(counts[i])).create();
       }
       var test = await runTest([
-        for (var i = 1; i <= 4; i++) 'test$i.dart',
+        for (var i = 0; i < counts.length; i++) 'test$i.dart',
         '-p',
         'vm',
         '-c',
         'exe',
       ], concurrency: 4);
 
-      expect(test.stdout, emitsThrough(contains('+4: All tests passed!')));
+      expect(test.stdout, emitsThrough(contains('All tests passed!')));
       await test.shouldExit(0);
     });
 
     test('can run chrome and vm exe suites concurrently', () async {
-      for (var i = 1; i <= 4; i++) {
-        await d.file('test$i.dart', _test).create();
+      final counts = [1, 50, 100, 200];
+      for (var i = 0; i < counts.length; i++) {
+        await d.file('test$i.dart', _testSuite(counts[i])).create();
       }
       var test = await runTest([
-        for (var i = 1; i <= 4; i++) 'test$i.dart',
+        for (var i = 0; i < counts.length; i++) 'test$i.dart',
         '-p',
         'chrome,vm',
         '-c',
         'dart2js,exe',
       ], concurrency: 8);
 
-      expect(test.stdout, emitsThrough(contains('+8: All tests passed!')));
+      expect(test.stdout, emitsThrough(contains('All tests passed!')));
       await test.shouldExit(0);
     }, tags: 'chrome');
 
     test(
       'can run chrome and vm cli suites concurrently',
       () async {
-        for (var i = 1; i <= 4; i++) {
-          await d.file('test$i.dart', _test).create();
+        final counts = [1, 50, 100, 200];
+        for (var i = 0; i < counts.length; i++) {
+          await d.file('test$i.dart', _testSuite(counts[i])).create();
         }
         var test = await runTest([
-          for (var i = 1; i <= 4; i++) 'test$i.dart',
+          for (var i = 0; i < counts.length; i++) 'test$i.dart',
           '-p',
           'chrome,vm',
           '-c',
           'dart2js,cli',
         ], concurrency: 8);
 
-        expect(test.stdout, emitsThrough(contains('+8: All tests passed!')));
+        expect(test.stdout, emitsThrough(contains('All tests passed!')));
         await test.shouldExit(0);
       },
       tags: 'chrome',
