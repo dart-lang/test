74986df79a1a1af119e0841dcdc253b2e8bca716
Nate Bosch <nbosch@google.com>
2026-09-17 00:37:06 +0000

    No need to manually hold test open

--- pkgs/checks/example/async/future/does_not_complete.dart
+++ pkgs/checks/example/async/future/does_not_complete.dart
@@ -9,11 +9,10 @@ void main() {
     check(Completer<int>().future).doesNotComplete;
 
     // This check fails. The failure is reported asynchronously, after the
-    // future completes, so the test must stay running until then.
+    // future completes.
     final completer = Completer<int>();
     check(completer.future).doesNotComplete;
     completer.complete(42);
-    await completer.future;
     // Expected: a Future<int> that:
     //   does not complete
     // Actual: a future that completed to <42>
